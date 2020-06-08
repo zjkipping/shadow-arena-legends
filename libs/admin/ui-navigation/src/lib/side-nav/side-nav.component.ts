c0180c93997757a26afe7b7d7f83fced8816e4da
Zachary Kipping <zjkipping@hotmail.com>
2020-06-07 23:17:01 -0500

    SAL-12 Cleaned up icon colors, Added animation to side nav

--- libs/admin/ui-navigation/src/lib/side-nav/side-nav.component.ts
+++ libs/admin/ui-navigation/src/lib/side-nav/side-nav.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  group,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { NavLink } from '@shadow-arena-legends/shared/util-types';
@@ -6,6 +13,24 @@ import { NavLink } from '@shadow-arena-legends/shared/util-types';
   selector: 'sal-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss'],
+  animations: [
+    trigger('navSlide', [
+      transition(':enter', [
+        style({ opacity: 0, width: 0 }),
+        group([
+          animate('200ms', style({ opacity: 1 })),
+          animate('100ms', style({ width: '*' })),
+        ]),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, width: '*' }),
+        group([
+          animate('100ms', style({ opacity: 0 })),
+          animate('200ms', style({ width: '0' })),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class SideNavComponent {
   @Input() links: NavLink[] = [];
